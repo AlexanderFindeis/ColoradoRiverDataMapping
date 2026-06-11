762170921ad7255a59d50f896693d62f986839c4
AlexanderFindeis <117101737+AlexanderFindeis@users.noreply.github.com>
2026-06-11 10:43:19 -0700

    Scheduled Data Update: Thu 06/11/2026 10:43:19.64

--- Data_Updates.bat
+++ Data_Updates.bat
@@ -3,14 +3,14 @@
 set RSCRIPT="C:\Program Files\R\R-4.2.3\bin\Rscript.exe"
 set quarto="C:\Users\a1928\AppData\Local\Programs\Positron\resources\app\quarto\bin\quarto.exe"
 
-echo === Running R data scripts ===
-%Rscript% Scripts/Reservoir_Data.R
-%Rscript% Scripts/Snow_Pack_Data.R
+@REM echo === Running R data scripts ===
+@REM %Rscript% Scripts/Reservoir_Data.R
+@REM %Rscript% Scripts/Snow_Pack_Data.R
 
 echo === Rendering Quarto pages ===
-quarto render index.qmd
-quarto render "Pages/Reservoirs/Reservoir Overview.qmd"
-quarto render "Pages/Snow Pack/Snow Pack.qmd"
+quarto.cmd render index.qmd
+quarto.cmd render "Pages/Reservoirs/Reservoir Overview.qmd"
+quarto.cmd render "Pages/Snow Pack/Snow Pack.qmd"
 
 echo === Committing and pushing changes ===
 git add .
