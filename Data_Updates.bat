e0a1938dc1ed88e59939dec402e887ba67221b5f
AlexanderFindeis <117101737+AlexanderFindeis@users.noreply.github.com>
2026-06-11 11:04:43 -0700

    Updated .bat file

--- Data_Updates.bat
+++ Data_Updates.bat
@@ -3,9 +3,9 @@
 set RSCRIPT="C:\Program Files\R\R-4.2.3\bin\Rscript.exe"
 set quarto="C:\Users\a1928\AppData\Local\Programs\Positron\resources\app\quarto\bin\quarto.exe"
 
-@REM echo === Running R data scripts ===
-@REM %Rscript% Scripts/Reservoir_Data.R
-@REM %Rscript% Scripts/Snow_Pack_Data.R
+echo === Running R data scripts ===
+%Rscript% Scripts/Reservoir_Data.R
+%Rscript% Scripts/Snow_Pack_Data.R
 
 echo === Rendering Quarto pages ===
 %quarto% render index.qmd
