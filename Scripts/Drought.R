9318654cf1a82a7fd0a956b83d295d609a53d1af
AlexanderFindeis <117101737+AlexanderFindeis@users.noreply.github.com>
2026-06-23 10:35:28 -0700

    Scheduled Data Update: Tue 06/23/2026 10:35:28.60

--- Scripts/Drought.R
+++ Scripts/Drought.R
@@ -1,3 +1,5 @@
+# !! -- Not currently in use for processing. See Drought.qmd -- !!
+
 library(dplyr)
 library(sf)
 
@@ -20,7 +22,17 @@ library(dplyr)
 geo <- sf::st_read("/vsizip//vsicurl/https://droughtmonitor.unl.edu/data/shapefiles_m/USDM_current_M.zip")
 st_write(geo, "GIS_Data/Current_Drought.geojson", delete_dsn = TRUE)
 
-UB <- st_read("GIS_Data/UpperBasin_HUC2.geojson")
-LB <- st_read("GIS_Data/LowerBasin_HUC2.geojson")
+DM <- st_read("GIS_Data/Current_Drought.geojson") %>%
+  st_transform(4326)
+UB <- st_read("GIS_Data/UpperBasin_HUC2.geojson") %>%
+  st_transform(4326)
+LB <- st_read("GIS_Data/LowerBasin_HUC2.geojson") %>%
+  st_transform(4326)
 
+D0 <- geo %>% filter(DM == 0)
+D1 <- geo %>% filter(DM == 1)
+D2 <- geo %>% filter(DM == 2)
+D3 <- geo %>% filter(DM == 3)
+D4 <- geo %>% filter(DM == 4)
 
+UB_D0 <- st_intersection(UB, D0)
